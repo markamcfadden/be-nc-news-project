ac98849f6398d448b84baf8b3079131c57ef23c7
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-27 15:04:36 +0000

    add endpoint /api/articles/:article_id and error handling

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const { getEndpoints } = require("./controllers/api-controller");
 const { getTopics } = require("./controllers/topics-controller");
+const { getArticleByID } = require("./controllers/articles.controller");
 const app = express();
 
 app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_ID", getArticleByID);
+
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err === "article does not exist") {
+    res.status(404).send({ msg: err });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  }
 });
 
 module.exports = app;
