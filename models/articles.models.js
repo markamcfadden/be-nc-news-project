9e3a203a33bfd4f11bd5b82b57e673b2ab2913b7
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-27 17:21:42 +0000

    add selectArticles model

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,5 +1,15 @@
 const db = require("../db/connection");
 
+exports.selectArticles = () => {
+  return db
+    .query(
+      "SELECT articles.article_id, title, topic, articles.author, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC"
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
 exports.selectArticleByID = (id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [id])
