c7a38c2d94534cd210319cd6ef9c9d068ee06396
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-29 17:02:58 +0000

    changed test to return 201 for a patch if there are additional fields

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -468,14 +468,25 @@ describe("PATCH /api/articles/:article_id", () => {
         expect(body.msg).toBe("Bad request, missing required fields");
       });
   });
-  test("400: should return an error if there are unexpected field", () => {
+  test("201: should return an updated article with the votes property adjusted accordingly even if other fields are present", () => {
     const votesToAdd = { inc_votes: 15, username: "butter_bridge" };
     return request(app)
       .patch("/api/articles/1")
       .send(votesToAdd)
-      .expect(400)
+      .expect(200)
       .then(({ body }) => {
-        expect(body.msg).toBe("Bad request, unexpected fields");
+        const updatedArticle = body.updatedArticle[0];
+        expect(updatedArticle).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: expect.any(String),
+          votes: 115,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
       });
   });
 });
