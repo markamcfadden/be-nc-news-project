60cea98dcf2096b793aed12be7f3faa06e6b6095
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-29 09:25:59 +0000

    add util func to check comment exists

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -49,3 +49,15 @@ exports.checkUsernameExists = (username) => {
       }
     });
 };
+
+exports.checkCommentExists = (comment_id) => {
+  return db
+    .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "comment does not exist" });
+      } else {
+        return;
+      }
+    });
+};
