b1affd13e0cff7e4a6acc40a002026a494b23b37
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-29 17:04:03 +0000

    removed logic in updateArticleByID controller to cause an error if there are unneccessary fields and instead return a 201

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -25,15 +25,6 @@ exports.updateArticleByID = (req, res, next) => {
   const { article_ID } = req.params;
   const { inc_votes } = req.body;
 
-  const expectedFields = ["inc_votes"];
-  const recievedFields = Object.keys(req.body);
-  const unexpectedFields = recievedFields.filter(
-    (field) => !expectedFields.includes(field)
-  );
-  if (unexpectedFields.length > 0) {
-    return res.status(400).send({ msg: "Bad request, unexpected fields" });
-  }
-
   return patchArticleByID(article_ID, inc_votes)
     .then((updatedArticle) => {
       res.status(200).send({ updatedArticle });
