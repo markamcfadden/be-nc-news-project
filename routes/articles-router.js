e84a5f3e11ad429bdce905f54e33a6798dd7b649
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-30 19:45:37 +0000

    add endpoint POST /api/articles

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -3,12 +3,14 @@ const {
   getArticles,
   getArticleByID,
   updateArticleByID,
+  postArticle,
 } = require("../controllers/articles.controller");
 const commentsRouter = require("./comments-router");
 
 const articlesRouter = express.Router();
 
 articlesRouter.get("/", getArticles);
+articlesRouter.post("/", postArticle);
 articlesRouter.get("/:article_id", getArticleByID);
 articlesRouter.patch("/:article_id", updateArticleByID);
 
