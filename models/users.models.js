dbb4c393aeb5205e06a7fa26e358a9978163e238
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-30 15:02:35 +0000

    add model selectUserByUsername

--- models/users.models.js
+++ models/users.models.js
@@ -1,7 +1,18 @@
 const db = require("../db/connection");
+const { checkUsernameExists } = require("../db/seeds/utils");
 
 exports.selectUsers = () => {
   return db.query(`SELECT * FROM users`).then(({ rows }) => {
     return rows;
+  });
+};
+
+exports.selectUserByUsername = (username) => {
+  return checkUsernameExists(username).then(() => {
+    return db
+      .query(`SELECT * FROM users WHERE username = $1`, [username])
+      .then(({ rows }) => {
+        return rows[0];
+      });
   });
 };
