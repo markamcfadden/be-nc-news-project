4bab87216d0956d39b4d9e42ff2684271a799f15
markamcfadden <mcfadden.a.mark@gmail.com>
2025-01-31 12:40:01 +0000

    add model insertTopic

--- models/topics-models.js
+++ models/topics-models.js
@@ -5,3 +5,28 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.insertTopic = (slug, description) => {
+  if (!slug || !description) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request, missing required fields",
+    });
+  }
+  if (typeof slug !== "string" || typeof description !== "string") {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request, invalid data type",
+    });
+  }
+
+  return db
+    .query(
+      `INSERT into topics (slug, description)
+    VALUES ($1, $2) RETURNING *`,
+      [slug, description]
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
