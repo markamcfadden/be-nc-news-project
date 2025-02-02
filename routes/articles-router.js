7fd237ab4625ddf7cf655d22b4b87f977dd8b2d4
markamcfadden <mcfadden.a.mark@gmail.com>
2025-02-02 13:29:57 +0000

    add endpoint DELETE /api/articles/:article_id

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -4,6 +4,7 @@ const {
   getArticleByID,
   updateArticleByID,
   postArticle,
+  deleteArticleAndCommentsByID,
 } = require("../controllers/articles.controller");
 const commentsRouter = require("./comments-router");
 
@@ -13,6 +14,7 @@ articlesRouter.get("/", getArticles);
 articlesRouter.post("/", postArticle);
 articlesRouter.get("/:article_id", getArticleByID);
 articlesRouter.patch("/:article_id", updateArticleByID);
+articlesRouter.delete("/:article_id", deleteArticleAndCommentsByID);
 
 articlesRouter.use("/:article_id/comments", commentsRouter);
 
